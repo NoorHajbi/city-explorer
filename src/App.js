0c80d8a7297116ddc73d4840c2346dc39a1fe7de
Noor Jamal Hajbi <noor.hajbi@gmail.com>
2021-05-24 01:50:08 +0300

    day1 finsihed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      citySearched: '',
+      output: '',
+      show: false,
+      errorMessage: false,
+      apiKey: `pk.b2d1d28fe6236b24f76b7d5f8e2403d6`
+    }
+  }
+
+  getLocation = async (e) => {
+    e.preventDefault();
+    let LocUrl = `https://eu1.locationiq.com/v1/search.php?key=${this.state.apiKey} &q=${this.state.citySearched}&format=json`;
+
+    try {
+      let locResult = await axios.get(LocUrl);
+      this.setState({
+        output: locResult.data[0],
+        show: true,
+      })
+      // console.log(this.state.output);
+    }
+    catch {
+      this.setState({
+        show: false,
+        errorMessage: true
+      })
+    }
+  }
+
+  updateSearch = (event) => {
+    this.setState({
+      citySearched: event.target.value,
+    })
+    // console.log(this.state.citySearched);
+  }
+  render() {
+    return (
+      <>
+        <h1>City Explorer</h1>
+        <form onSubmit={this.getLocation}>
+          <input type='text' placeholder='add a city' onChange={this.updateSearch} />
+          <button value='Get Location'>Explore!</button>
+        </form>
+
+        { this.state.show &&
+          <p>
+            City:<span>{this.state.output.display_name}</span> <br />
+          Lat / Long: <span>{this.state.output.lat}, {this.state.output.lon}</span></p>
+        }
+
+        { this.state.show &&
+          <img
+            src={`https://maps.locationiq.com/v3/staticmap?key=${this.state.apiKey}&center=${this.state.output.lat},${this.state.output.lon}`} alt=''
+          />
+        }
+        { this.state.errorMessage &&
+          <p>
+            "error": "Unable to geocode"
+          </p>
+        }
+      </>
+    )
+  }
 }
 
 export default App;
