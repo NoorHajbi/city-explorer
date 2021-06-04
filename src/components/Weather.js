635bbe0b775a8745abf83719be7a5d36a2063be8
Noor Jamal Hajbi <noor.hajbi@gmail.com>
2021-06-05 01:57:12 +0300

    finish lab09

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import WeatherDay from './WeatherDay'
+
 class Weather extends React.Component {
   render() {
     return (
       this.props.weatherData.map(item => {
         return (
           <>
-            <p> Date:<span>{item.date} </span> <br />
-            Description:<span>{item.description}</span></p>
+            {/* <p> Date:<span>{item.date} </span> <br />
+            Description:<span>{item.description}</span></p> */}
+
+            <WeatherDay
+              date={item.date}
+              description={item.description} />
           </>
         );
       })
